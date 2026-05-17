902ec2c02d574cab93496d8d61a0cb425a83b9d4
Your Name <your-email@example.com>
2026-05-17 20:50:52 +0700

    first commit

---
+++ src/main/java/com/example/it211_session02_bai02/It211Session02Bai02Application.java
@@ -0,0 +1,13 @@
+package com.example.it211_session02_bai02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It211Session02Bai02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(It211Session02Bai02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/it211_session02_bai02/It211Session02Bai02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.it211_session02_bai02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It211Session02Bai02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
